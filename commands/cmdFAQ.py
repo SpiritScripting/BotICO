7169358c25482e6e983ca972d2b35b34997083ee
SpitefulGrog <kev_2108@outlook.com>
2017-10-25 22:24:36 -0600

    Enable the FAQ module

--- commands/cmdFAQ.py
+++ commands/cmdFAQ.py
@@ -1,14 +1,20 @@
 def get_faq(bot, update, args):
 	import yaml
-	faq_file = '../config/faq.yml'
+	faq_file = 'config/faq.yml'
 	score_dict = {'':''}
 	with open(faq_file, 'r') as ymlfile:
 		cfg = yaml.load(ymlfile)
 		for id in cfg:
+			wordcount = 0
 			for item in args:
-				wordcount = 0
 				if item in cfg[id]['words']:
 					wordcount += 1
-#					score_dict[cfg[id]['id']] = 
-			answer=cfg[id]['answer']
-			bot.sendMessage(update.message.chat_id, text=answer)
+			score_dict[cfg[id]['id']] = wordcount
+		max = 0
+		for key, value in score_dict.items():
+			if value != '':
+				if int(value) > max:
+					max = int(value)
+					key_max = int(key)
+		answer=cfg[key_max]['answer']
+	bot.sendMessage(update.message.chat_id, text=answer)
